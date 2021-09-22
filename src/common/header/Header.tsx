6693e895c98c1eaf0490f23970565e38683a5d10
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2021-09-22 17:34:43 -0300

    redirect to homepage on header click

--- src/common/header/Header.tsx
+++ src/common/header/Header.tsx
@@ -5,7 +5,9 @@ import Logo from "../../assets/logo.png";
 const Header: React.FC = () => {
   return (
     <div className="header">
-      <img className="header-logo" src={Logo} alt={"logo bazar"} />
+      <a href={"/"}>
+        <img className="header-logo" src={Logo} alt={"logo bazar"} />
+      </a>
     </div>
   );
 };
